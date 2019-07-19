b085e22c154e18d87b95691ae3e9640ca0879ae7
Wesley-M <wesley.santos@ccc.ufcg.edu.br>
2019-07-19 16:58:10 -0300

    master.css was formatted, positioning bug of scrollspy options fixed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 window.onload = () => {
-
-  var BODY_RECT = document.body.getBoundingClientRect();
-  var EDUCATIONS_POSITION = document.querySelector('#educations').getBoundingClientRect().top - BODY_RECT.too - 300;
-  var ABILITIES_POSITION = document.querySelector('#abilities').getBoundingClientRect().top - BODY_RECT.top - 300;
-  var CONTACTS_POSITION = document.querySelector('#contacts').getBoundingClientRect().top - BODY_RECT.top - 300;
-
   // Scroll to element position
   var elements = document.getElementsByClassName("option-spy");
   for (var i = 0; i < elements.length; i++) {
@@ -12,17 +6,27 @@ window.onload = () => {
   }
 
   // Set 'active' class in the current appearing section
+  setActive();
   window.addEventListener("scroll", () => {
-    removeActive();
-    offsetY = window.pageYOffset;
-    if (offsetY < ABILITIES_POSITION) {
-      document.getElementsByName("educations")[0].classList.toggle("active");
-    } else if (offsetY < CONTACTS_POSITION) {
-      document.getElementsByName("abilities")[0].classList.toggle("active");
-    } else {
-      document.getElementsByName("contacts")[0].classList.toggle("active");
-    }
+    setActive();
   });
+}
+
+function setActive() {
+  var BODY_RECT = document.body.getBoundingClientRect();
+  var EDUCATIONS_POSITION = document.querySelector('#educations').getBoundingClientRect().top - BODY_RECT.too - 300;
+  var ABILITIES_POSITION = document.querySelector('#abilities').getBoundingClientRect().top - BODY_RECT.top - 300;
+  var CONTACTS_POSITION = document.querySelector('#contacts').getBoundingClientRect().top - BODY_RECT.top - 300;
+
+  removeActive();
+  offsetY = window.pageYOffset;
+  if (offsetY < ABILITIES_POSITION) {
+    document.getElementsByName("educations")[0].classList.toggle("active");
+  } else if (offsetY < CONTACTS_POSITION) {
+    document.getElementsByName("abilities")[0].classList.toggle("active");
+  } else {
+    document.getElementsByName("contacts")[0].classList.toggle("active");
+  }
 }
 
 function scrollTo() {
